47d09a21d96c8615bfdee7bfed81c9578fc88ab8
Ani Edith <ifeyinwaedith21@gmail.com>
2023-03-14 18:41:39 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -12,10 +12,12 @@ int main(void)
     char *s;
 
     s = str_concat("Betty ", "Holberton");
+
     if (s == NULL)
+
     {
-        printf("failed\n");
-        return (1);
+    printf("failed\n");
+    return (1);
     }
     printf("%s\n", s);
     free(s);
